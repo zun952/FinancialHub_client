10bd1cab1ffcff31d030a7ea26d27d708adedd0e
신동민 <gofire99@naver.com>
2021-07-19 00:53:15 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CoinsComponent } from './coins/coins.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CoinsComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule,
